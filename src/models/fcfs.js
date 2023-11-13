7a276c5e1e7efc354f21d71dda5268c180f637d2
cifelse <meetcif@gmail.com>
2023-11-13 18:00:12 +0800

    Add FCFS algo

---
+++ src/models/fcfs.js
@@ -0,0 +1,11 @@
+export const name = `First Come First Serve`;
+
+/**
+ * First Come First Serve Algorithm
+ * @module models/fcfs
+ * @async
+ * @method
+ */
+export const execute = async () => {
+    // TODO
+}
