c857ca54bddfca686a06678ef5c5f2bd0c8566eb
cifelse <meetcif@gmail.com>
2023-11-13 18:00:51 +0800

    Add SRTF algo

---
+++ src/models/srtf.js
@@ -0,0 +1,11 @@
+export const name = `Shortest Remaining Time First`;
+
+/**
+ * First Come First Serve Algorithm
+ * @module models/srtf
+ * @async
+ * @method
+ */
+export const execute = async () => {
+    // TODO
+}
