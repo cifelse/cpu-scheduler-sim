7d7fd30115c47f9f5cd2abe9d476160ad4718681
cifelse <meetcif@gmail.com>
2023-11-13 18:00:34 +0800

    Add RR algo

---
+++ src/models/rr.js
@@ -0,0 +1,11 @@
+export const name = `Round Robin`;
+
+/**
+ * First Come First Serve Algorithm
+ * @module models/rr
+ * @async
+ * @method
+ */
+export const execute = async () => {
+    // TODO
+}
