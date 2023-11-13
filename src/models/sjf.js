d0cbefa260fe8c17af1d4f4ca5f4fca7fbc429b2
cifelse <meetcif@gmail.com>
2023-11-13 18:00:40 +0800

    Add SJF algo

---
+++ src/models/sjf.js
@@ -0,0 +1,11 @@
+export const name = `Shortest Job First`;
+
+/**
+ * First Come First Serve Algorithm
+ * @module models/sjf
+ * @async
+ * @method
+ */
+export const execute = async () => {
+    // TODO
+}
