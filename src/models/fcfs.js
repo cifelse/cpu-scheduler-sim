099d01094f797adb506391d272298b50997ce163
cifelse <meetcif@gmail.com>
2023-11-17 12:19:19 +0800

    Finish FCFS

--- src/models/fcfs.js
+++ src/models/fcfs.js
@@ -18,34 +18,50 @@ export const execute = async (Y) => {
     // Get the processes from the user
     const processes = await getProcesses(Y);
     
+    const results = await fcfs(processes);
+
+    return await display(results);
+}
+
+/**
+ * First Come First Serve Algorithm
+ * @async
+ * @method
+ * @param {Array<Object>} processes - Array of Processes
+ * @returns {Array<Object>} - Array of Results
+ */
+export const fcfs = async (processes) => {
     // Sort by Arrival Time
     processes.sort((a, b) => a.arrival - b.arrival);
 
     let currentTime = 0, totalWaitTime = 0, results = [];
 
+    // Algorithm Proper
     for (const process of processes) {
         // Calculate start time (max of current time and arrival time)
         const startTime = Math.max(currentTime, process.arrival);
 
         // Calculate end time
-        const endTime = startTime + process.burst;
+        currentTime = startTime + process.burst;
 
         // Calculate waiting time
         const waitingTime = startTime - process.arrival;
 
         // Track the process details
-        results.push(`${process.id} start time: ${startTime} end time: ${endTime} | Waiting time: ${waitingTime}`);
+        results.push({ id: process.id, details: `start time: ${startTime} end time: ${currentTime} | Waiting time: ${waitingTime}` });
 
         // Update total waiting time and current time
         totalWaitTime += waitingTime;
-        currentTime = endTime;
     };
 
-    // Calculate and print the average waiting time
+    // Calculate average waiting time
     const averageWaitingTime = totalWaitTime / processes.length;
+
+    // Sort by id
+    results = results.sort((a, b) => a.id - b.id).map(r => { return `${r.id} ${r.details}` });
 
     // Track the average waiting time
     results.push(`Average waiting time: ${averageWaitingTime.toFixed(1)}`);
 
-    return await display(results);
+    return results;
 }
